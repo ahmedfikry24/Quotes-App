2412c68d1c143d57672e45a30493dcb9a5da6789
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2022-12-19 14:34:06 +0200

    create project structure

---
+++ lib/features/random_qoutes/presentation/cubit/random_qoutes_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+part 'random_qoutes_state.dart';
+
+class RandomQoutesCubit extends Cubit<RandomQoutesState> {
+  RandomQoutesCubit() : super(RandomQoutesInitial());
+}
